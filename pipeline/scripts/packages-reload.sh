09a0050f51d21a2597e475e4ddf02fbce486bc50
ponchotitlan <asandovalros@gmail.com>
2025-06-03 10:26:24 +0100

    Updated documentation for SWAT Overflow

--- pipeline/scripts/packages-reload.sh
+++ pipeline/scripts/packages-reload.sh
@@ -12,9 +12,9 @@ container_name=$("$NSO_DOCKER_NAME_GEN")
 
 reload_output=$(docker exec -i $container_name bash -lc "echo 'packages reload' | ncs_cli -Cu admin")
 if echo "$reload_output" | grep -q "$TOKEN_FAILED"; then
-    # This test passed!
+    # Packages reload failed!
     echo "failed"
 else
-    # This test didn't pass!
+    # Packages reload passed!
     echo "pass"
 fi
